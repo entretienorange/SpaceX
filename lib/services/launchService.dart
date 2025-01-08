c4a041e4a3afe6685cb3550ce626fc9cf72765bb
23karim <karim.kekli@esprit.tn>
2025-01-08 14:08:46 +0100

    Add la gestion des erreurs

--- lib/services/launchService.dart
+++ lib/services/launchService.dart
@@ -2,30 +2,39 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:spacex/models/launches.dart';
 import 'package:spacex/services/apiConstants.dart';
-
+import 'dart:io';
 
 class LaunchService {
+
   Future<List<Launch>> fetchLaunches() async {
-  final url = Uri.parse('${ApiConstants.baseUrl}/launches');
-  try {
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      return data.map((json) {
-        try {
-          return Launch.fromJson(json);
-        } catch (e) {
-          print('Erreur de parsing pour l\'élément $json : $e');
-          throw Exception('Erreur dans le parsing des données');
-        }
-      }).toList();
-    } else {
-      throw Exception('Erreur de récupération: ${response.statusCode}');
+    final url = Uri.parse('${ApiConstants.baseUrl}/launches');
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+
+        return data.map((json) {
+          try {
+            return Launch.fromJson(json);
+          } catch (e) {
+            print('Erreur de elément $json : $e');
+            throw Exception('Erreur d données');
+          }
+        }).toList();
+      } else {
+        throw Exception('Erreur de récupération des données : ${response.statusCode}');
+      }
+    } catch (e) {
+      if (e is SocketException) {
+        throw Exception('Erreur de connexion');
+      } else if (e is FormatException) {
+        throw Exception('Erreur de format des données');
+      } else {
+        throw Exception('Une erreur inconn: $e');
+      }
     }
-  } catch (e) {
-    throw Exception('Erreur de récupération : $e');
   }
-}
 
   Future<Launch> fetchLaunchById(String id) async {
     final url = Uri.parse('${ApiConstants.baseUrl}/launches/$id');
@@ -35,10 +44,16 @@ class LaunchService {
         final data = json.decode(response.body);
         return Launch.fromJson(data);
       } else {
-        throw Exception('Erreur de récupération: ${response.statusCode}');
+        throw Exception('Erreur de récupération des détails $id : ${response.statusCode}');
       }
     } catch (e) {
-      throw Exception('Erreur de récupération : $e');
+      if (e is SocketException) {
+        throw Exception('Erreur de connexion');
+      } else if (e is FormatException) {
+        throw Exception('Erreur de format des données');
+      } else {
+        throw Exception('Une erreur inconn: $e');
+      }
     }
   }
 }
--- lib/services/missionService.dart
+++ lib/services/missionService.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:spacex/models/missions.dart';
 import 'package:spacex/services/apiConstants.dart';
+import 'dart:io';
 
 class MissionService {
   Future<List<Mission>> fetchMissions() async {
@@ -10,33 +11,47 @@ class MissionService {
       final response = await http.get(url);
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body);
-        // Log the raw response data
-        print("Raw response data: $data");
-        return data.map((json) => Mission.fromJson(json)).toList();
+        print("Donnees de la réponse: $data");
+        return data.map((json) {
+          try {
+            return Mission.fromJson(json);
+          } catch (e) {
+            print('Erreur de elément $json : $e');
+            throw Exception('Erreur d données');
+          }
+        }).toList();
       } else {
-        throw Exception('err fetch missions: ${response.statusCode}');
+        throw Exception('Erreur de récupération des missions : ${response.statusCode}');
       }
     } catch (e) {
-      print("Error: $e");
-      throw Exception('err fetching: $e');
+      if (e is SocketException) {
+        throw Exception('Erreur de connexion');
+      } else if (e is FormatException) {
+        throw Exception('Erreur de format des données');
+      } else {
+        throw Exception('Une erreur inconn : $e');
+      }
     }
   }
-
   Future<Mission> fetchMissionById(String id) async {
     final url = Uri.parse('${ApiConstants.baseUrl}/missions/$id');
     try {
       final response = await http.get(url);
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = json.decode(response.body);
-        // Log the raw response data
-        print("Raw response data by ID: $data");
+        print("Donnée de  réponse  $data");
         return Mission.fromJson(data);
       } else {
-        throw Exception('err fetch details: ${response.statusCode}');
+        throw Exception('Erreur de récupération des détails  $id : ${response.statusCode}');
       }
     } catch (e) {
-      print("Error: $e");
-      throw Exception('err fetch details: $e');
+      if (e is SocketException) {
+        throw Exception('Erreur de connexion');
+      } else if (e is FormatException) {
+        throw Exception('Erreur de format des données');
+      } else {
+        throw Exception('Une erreur inconn: $e');
+      }
     }
   }
 }
